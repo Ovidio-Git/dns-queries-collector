440923606bed19115f937496f1fa20854ee7fe6b
Ovidio Andrade <oviidio.andrade@gmail.com>
2024-02-09 17:06:14 -0500

    add stats view and logs buttons

--- app/app.py
+++ app/app.py
@@ -1,33 +1,45 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for
 from os import getenv
 from requests import post 
-from .utils.helper import send_to_lumu_api 
+from utils.helper import send_to_lumu 
 
 app = Flask(__name__)
 app.secret_key = getenv('FLASK_SECRET_LUMU')
-
+ 
 # Define main path
 @app.route('/', methods=['GET', 'POST'])
-def upload_file():
+def main():
+    return render_template('index.html')
+  
+# Define result path with statics
+@app.route('/send', methods=['POST'])
+def send_log():
     if request.method == 'POST':
-        # check if the post request has the file part
+        print("Files:", request.files)
+        print("Form:", request.form)
+        
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+            print('No file part')
+            return render_template('index.html')
+        
         file = request.files['file']
         if file:
-            total_lines = send_to_lumu_api(file)  # Procesa y obtiene el total de líneas
-            flash(f'Archivo procesado con éxito. Total de líneas: {total_lines}')
-            return redirect(url_for('result'))
+            total_lines = send_to_lumu(file)
+            print('Total lines:', total_lines)
+            total_records = 6000
+            client_ips_rank = {
+                '45.238.196.2': {'hits': 426, 'percentage': 7.10},
+            }
+            host_rank = {
+                'TTRTX.local': {'hits': 305, 'percentage': 5.08},
+            }
+            return render_template('stats.html', total_records=total_records, client_ips_rank=client_ips_rank, host_rank=host_rank)
     else:
         return render_template('index.html')
-
-
-
-# Define result path with statics
-@app.route('/result')
-def result_upload():
-    return 'test'
+ 
+@app.route('/logs')
+def logs_app():
+    return 'logs'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8043)
--- app/utils/helper.py
+++ app/utils/helper.py
@@ -3,34 +3,57 @@ from os import getenv
 from json import dumps
 from logs.manager import logger 
 
+# Ejecuta el comando 'ls' y captura la salida
+
+
 collector_id = getenv('LUMU_COLLECTOR_ID')
 lumu_client_key = getenv('LUMU_CLIENT_KEY')
-URL_TARGET = f"http://example.com/collectors/{collector_id}/dns/queries?key={lumu_client_key}"
-    
-def send_to_api(payload: dict) -> bool:
+#URL_TARGET = f"http://example.com/collectors/{collector_id}/dns/queries?key={lumu_client_key}"
+URL_TARGET = ''
 
+def send_to_api(payload: dict) -> bool:
+    """ Send the payload to the Lumu API and return True if the action was successful
+        Args:
+            payload (dict): The payload to send to the Lumu API
+        Returns:
+            bool: True if the action was successful, False otherwise"""
     headers = {'Content-Type': 'application/json'}
     try:
         response = post(URL_TARGET, headers=headers, data=dumps(payload))
         if response.status_code == 200:
             logger.info("Data send to Lumu API successfully")
+            print("Data send to Lumu API successfully")
         else:
             logger.error(f"Status code: {response.status_code}, Response: {response.text}")
         return True
     except Exception as e:
         logger.error(f"Exception: {e}")
         return False
-
-
-
-def parse_log_file(file_path):
+        
+def parse_log_file(chunk_raw) -> list[dict]:
     """ Process the log file with a hunk size of 500
-
         Args:
-            file_path (str): The path of the file to process"""
-    return True  # Devuelve si la acción fue exitosa
-
-def send_to_lumu_api(file_stream):
+            file_path (str): The path of the file to process
+        Returns:
+            list[dict]: The processed data in a list of dictionaries"""
+    chunk_cooked = []
+    for line_bytes in chunk_raw[:10]:
+        # Convert the line to a string and split it by spaces
+        line_string = line_bytes.decode('utf-8')
+        data = line_string.split(" ")
+        print(data)
+        # Append the processed data to the chunk_cooked list
+        chunk_cooked.append({
+            "timestamp": data[0] + "T" + data[1],
+            "name": data[9],
+            "client_ip": data[6],
+            "client_name": None,
+            "type": data[11]
+        })
+    print(chunk_cooked)
+    return chunk_cooked
+ 
+def send_to_lumu(file_stream):
     """ Process the log file with a chunk size of 500 and send it to the Lumu API
     
         Args:
@@ -40,15 +63,13 @@ def send_to_lumu_api(file_stream):
     chunk_size = 500
     # Read the file lines
     lines = file_stream.readlines()
+    total_lines = len(lines)
+
     # Process the file in chunks
-    for i in range(0, len(lines), chunk_size):
+    for i in range(0, total_lines, chunk_size):
         # Get the chunk of lines to process 
         chunk = lines[i:i+chunk_size]
-        print(f"Procesando bloque de {len(chunk)} líneas")  
+        chunk_payload = parse_log_file(chunk)
+        send_to_api(chunk_payload)
 
-    return len(lines)  
-
-
-    # format: [ { "timestamp": "2021-01-06T14:37:02.228Z", "name": "www.example.com", "client_ip": "192.168.0.103", "client_name": "MACHINE-0987", "type": "A" } ]
-    # enpoint: POST /collectors/{collector-id}/dns/queries?key={lumu-client-key}
-    pass
+    return total_lines
