435ad4cd56d864996c9ec3901a8a81fa9dad2e83
Ovidio Andrade <oviidio.andrade@gmail.com>
2024-02-09 19:27:53 -0500

    DevOAndrade: updating comments before api test

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, render_template
 from os import getenv
-from requests import post 
 from utils.helper import send_to_lumu, read_logs
 
 app = Flask(__name__)
@@ -15,30 +14,22 @@ def main():
 @app.route('/send', methods=['POST'])
 def send_log():
     if request.method == 'POST':
-        print("Files:", request.files)
-        print("Form:", request.form)
-        
+        # check if the post request has the file part 
         if 'file' not in request.files:
             print('No file part')
             return render_template('index.html')
-        
+        # get the file from the request
         file = request.files['file']
         if file:
-            total_lines = send_to_lumu(file)
-            print('Total lines:', total_lines)
-            total_records = 6000
-            client_ips_rank = {
-                '45.238.196.2': {'hits': 426, 'percentage': 7.10},
-            }
-            host_rank = {
-                'TTRTX.local': {'hits': 305, 'percentage': 5.08},
-            }
-            return render_template('stats.html', total_records=total_records, client_ips_rank=client_ips_rank, host_rank=host_rank)
+            # send the file to the Lumu API and get the results stats
+            total_records, client_ips_rank, host_rank = send_to_lumu(file)
+            return render_template('stats.html', total_records=total_records, top_client_ips=client_ips_rank, top_hosts=host_rank)
     else:
         return render_template('index.html')
  
 @app.route('/logs')
 def logs_app():
+    # Read the logs and send them to the template
     inputs_logs = read_logs()
     return render_template('logs.html', inputs_logs=inputs_logs)
 
--- app/utils/helper.py
+++ app/utils/helper.py
@@ -3,14 +3,13 @@ from os import getenv
 from json import dumps
 from logs.manager import logger 
 from datetime import datetime
-
+from collections import Counter
 
 # Get the environment variables
 collector_id = getenv('LUMU_COLLECTOR_ID')
 lumu_client_key = getenv('LUMU_CLIENT_KEY')
 # Define the URL target
-#URL_TARGET = f"http://example.com/collectors/{collector_id}/dns/queries?key={lumu_client_key}"
-URL_TARGET = ''
+URL_TARGET = f"http://example.com/collectors/{collector_id}/dns/queries?key={lumu_client_key}"
 
 def read_logs() -> list[dict]:
     """Read the logs from the lumu.log file and return the data in a list of dictionaries
@@ -18,13 +17,13 @@ def read_logs() -> list[dict]:
             list[dict]: The logs data in a list of dictionaries"""
     inputs = []
     with open('./logs/lumu.log', 'r') as file:
-        for line in file.readlines()[-7:]:
+        for line in file:
             # Divide each line by the separator ' - ' and extract the relevant parts
             sections = line.strip().split(' - ')
             date = sections[0] 
             level = sections[1]  
             message = sections[2] 
-            # Append the extracted data to the inputs list
+            # Append the extracted data to the inputs list for the logs view
             inputs.append({'date': date, 'level': level, 'message': message})
     return inputs
 
@@ -34,8 +33,10 @@ def send_to_api(payload: dict) -> bool:
             payload (dict): The payload to send to the Lumu API
         Returns:
             bool: True if the action was successful, False otherwise"""
+    # Define the headers and send the payload to the Lumu API
     headers = {'Content-Type': 'application/json'}
     try:
+        # Send the payload to the Lumu API
         response = post(URL_TARGET, headers=headers, data=dumps(payload))
         if response.status_code == 200:
             logger.info("Data send to Lumu API successfully")
@@ -47,37 +48,73 @@ def send_to_api(payload: dict) -> bool:
         return False
  
 
-def parse_log_file(chunk_raw) -> list[dict]:
+def parse_log_file(chunk_raw,storage:list) -> tuple(list[dict], list[dict]):
     """ Process the log file with a hunk size of 500
         Args:
-            file_path (str): The path of the file to process
+            chunk_raw (list): The chunk of raw data to process
+            storage (list): The storage list to store the host and client IP for the stats
         Returns:
-            list[dict]: The processed data in a list of dictionaries"""
+            tuple: A tuple with the processed data and the storage list with the host and client IP for the stats"""
     chunk_cooked = []
-    for line_bytes in chunk_raw[:10]:
+                
+    for line_bytes in chunk_raw:
         # Convert the line to a string and split it by spaces
         line_string = line_bytes.decode('utf-8')
         data = line_string.split(" ")
-        # Append the processed data to the chunk_cooked list
+        # format the date to the API format
         date_base = data[0] + "T" + data[1]
         dt = datetime.strptime(date_base, "%d-%b-%YT%H:%M:%S.%f")
         date_format = dt.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+        # Extract the IP without the port
+        ip_without_port = data[6].split("#")[0]
+        # Append the processed data to the chunk_cooked list 
         chunk_cooked.append({
             "timestamp": date_format,
             "name": data[9],
-            "client_ip": data[6].split("#")[0],
+            "client_ip": ip_without_port,
             "client_name": None,
             "type": data[11]
         })
-    return chunk_cooked
- 
-def send_to_lumu(file_stream):
+        # Append the host and client IP to the storage list
+        storage.append({
+            "host": data[9],
+            "client_ip": ip_without_port,
+        })
+    return chunk_cooked, storage
+
+def get_stats(storage) -> tuple(int, list[tuple], list[tuple]):
+    """ Get the stats from the storage list
+        Args:
+            storage (list): The storage list with the host and client IP for the stats
+        Returns:
+            tuple: A tuple with the total records, the client IPs rank and the host rank in a dictionary format"""
+    # get the total records
+    total_records = len(storage)
+    # Get the hosts and client IPs
+    hosts = [item['host'] for item in storage]
+    client_ips = [item['client_ip'] for item in storage]
+    # Count the hosts and client IPs
+    host_counts = Counter(hosts)
+    client_ip_counts = Counter(client_ips)
+    # Get the top 5 hosts and client IPs
+    top_5_hosts = host_counts.most_common(5)
+    top_5_client_ips = client_ip_counts.most_common(5)
+    # Get the percentage of the top 5 hosts and client IPs
+    top_5_hosts_percent = [(host, count, f"{(count / total_records) * 100:.2f}%") for host, count in top_5_hosts]
+    top_5_client_ips_percent = [(ip, count, f"{(count / total_records) * 100:.2f}%") for ip, count in top_5_client_ips]
+
+    return total_records, top_5_client_ips_percent,top_5_hosts_percent
+            
+def send_to_lumu(file_stream) -> tuple(int, list[tuple], list[tuple]):
     """ Process the log file with a chunk size of 500 and send it to the Lumu API
     
         Args:
-            file_stream (file): The file to process"""
+            file_stream (file): The file to process
+        Returns:
+            tuple: A tuple with the total records, the client IPs rank and the host rank in a dictionary format"""
             
     # Define the chunk size
+    storage = []
     chunk_size = 500
     # Read the file lines
     lines = file_stream.readlines()
@@ -88,8 +125,8 @@ def send_to_lumu(file_stream):
         # Get the chunk of lines to process 
         chunk = lines[i:i+chunk_size]
         print("Chunk:", len(chunk))
-        chunk_payload = parse_log_file(chunk)
-        print("Chunk payload:", len(chunk_payload))
+        chunk_payload, storage = parse_log_file(chunk, storage)
+        print("Chunk payload:", chunk_payload)
         send_to_api(chunk_payload)
-
-    return total_lines
+    total_records, client_ips_rank, host_rank = get_stats(storage)
+    return total_records, client_ips_rank, host_rank
